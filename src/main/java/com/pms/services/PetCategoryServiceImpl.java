2cad5de75aca93dbd9f7715ad4bf5d7b1c811763
manishk199 <98672992+manishk199@users.noreply.github.com>
2025-04-11 12:53:01 +0530

    Update PetCategoryServiceImpl.java

--- src/main/java/com/pms/services/PetCategoryServiceImpl.java
+++ src/main/java/com/pms/services/PetCategoryServiceImpl.java
@@ -6,11 +6,12 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import com.pms.dao.PetCategoryRepository;
 import com.pms.exceptions.NotFoundException;
 import com.pms.pojos.PetCategory;
 
+@Transactional
 @Service
 public class PetCategoryServiceImpl implements IPetCategoryService {
 
