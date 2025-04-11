d9ae2e42f447afe147b19200725a9ba0bbf4dea6
manishk199 <98672992+manishk199@users.noreply.github.com>
2025-04-11 12:48:32 +0530

    Update PetServiceImpl.java

--- src/main/java/com/pms/services/PetServiceImpl.java
+++ src/main/java/com/pms/services/PetServiceImpl.java
@@ -7,11 +7,13 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pms.dao.PetRepository;
 import com.pms.exceptions.NotFoundException;
 import com.pms.pojos.Pet;
 
+@Transactional
 @Service
 public class PetServiceImpl implements IPetService{
 	@Autowired
